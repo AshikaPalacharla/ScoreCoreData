1e59f98612e661a0f532772371221147b5346ed2
Ashika Palacharla <ashika.palacharla@gmail.com>
2020-05-12 02:21:36 -0700

    Initial Commit

---
+++ ScoreCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScoreCoreData
+//
+//  Created by ashi on 5/12/20.
+//  Copyright © 2020 Ashika Palacharla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
