3ed5243fa4dc21d89215b293d4b23480f7990498
Ashika Palacharla <ashika.palacharla@gmail.com>
2020-05-12 02:47:22 -0700

    initial commit - lab working

--- ScoreCoreData/ViewController.swift
+++ ScoreCoreData/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    var score = Int() //variable for score set as integer
 
+    // Loads at initialization
     override func viewDidLoad() {
+        getData() //calls to get data method to get scores that are stored
+        scoreLabel.text = "\(score)"
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func increaseScore(sender: UIButton) {
+        score += 1
+        scoreLabel.text = "\(score)"
+    }
+    
+    @IBAction func saveScore(sender: UIButton) {
+        //method sends data up to core data storage
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "ScoreData", in: context)
+        //renaming ScoreData as entity
+        let newEntity = NSManagedObject(entity: entity!, insertInto: context)
+        //data will be saved up to replace base entity
+        
+        newEntity.setValue(score, forKey: "number")
+        
+        do {
+            try context.save()
+            print("saved + ur vibing")
+        } catch {
+            
+            print("failed to save + u messed up lol")
+        }
 
-
+    }
+    
+    func getData() {
+        //brings the data back down from the storage
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ScoreData")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request)
+            //fetches all the data in the array where you are storing
+            for data in result as! [NSManagedObject]
+            //setting the score to be the last item in the array
+            {
+                score = data.value(forKey: "number") as! Int
+            }
+        } catch {
+                print("data fetch failed")
+        }
+    }
+    
 }
 
